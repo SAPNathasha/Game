2804d145fd2f255e26b0fce6e613be4bf8b87999
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-03-10 23:48:42 +0530

    update js code

--- game.js
+++ game.js
@@ -1,18 +1,25 @@
-
 var btns = ["green","red","yellow","blue"];
-
-var randomBtnValue = Math.floor(Math.random()*4);
-var randomBtn = btns[randomBtnValue];
-
+var newBtn = [];
 var userSelection;
 var count = 0;
+var randomBtnValue;
+var randomBtn;
 
-$("#level-title").text("Press any key to start");
-$("document").on("keypress", function(){
+
+    $("#level-title").text("Press any key to start");
+
+    $("document").on("keypress", function(){
+        randomBtnValue = Math.floor(Math.random()*4);
+        randomBtn = btns[randomBtnValue];
+        randomBtn.hide(100).show(100);
+        var audio = new Audio("randomBtn".mp3);
+        audio.play();
+        newBtn.push(randomBtn);
+        });
+
     $("button").on("click", function(){
         userSelection = $(this).attr("id");
     });
-    
     
     if (userSelection === randomBtn){
         count++;
@@ -21,4 +28,4 @@ $("document").on("keypress", function(){
         $("#level-title").text("Game over. Press any key to start.");
         $("button").addClass("game-over");
     }
-})
+
