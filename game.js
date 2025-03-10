6855afefd03587d6a0f356a3b469a3f4db3171ba
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-03-11 00:09:40 +0530

    add multiple js functions

--- game.js
+++ game.js
@@ -8,24 +8,44 @@ var randomBtn;
 
     $("#level-title").text("Press any key to start");
 
-    $("document").on("keypress", function(){
-        randomBtnValue = Math.floor(Math.random()*4);
-        randomBtn = btns[randomBtnValue];
-        randomBtn.hide(100).show(100);
-        var audio = new Audio("randomBtn".mp3);
-        audio.play();
-        newBtn.push(randomBtn);
-        });
+    getBtns();
+    chechTheWin(randomBtn,userSelection);
 
-    $("button").on("click", function(){
-        userSelection = $(this).attr("id");
-    });
     
-    if (userSelection === randomBtn){
-        count++;
-        $("#level-title").text("Level "+count);
-    }else{
-        $("#level-title").text("Game over. Press any key to start.");
-        $("button").addClass("game-over");
+    function getBtns(){
+        $("document").on("keypress", function(){
+            randomBtnValue = Math.floor(Math.random()*4);
+            randomBtn = btns[randomBtnValue];
+            randomBtn.hide(100).show(100);
+           
+            newBtn.push(randomBtn);
+            });
+
+            $("button").on("click", function(){
+                userSelection = $(this).attr("id");
+            });
+
     }
 
+    function resetGame() {
+        level = 0;
+        newBtn = [];
+        started = false;
+    }
+
+    function playSound(name){
+        var audio = new Audio("randomBtn".mp3);
+        audio.play();
+    }
+
+    function chechTheWin(a,b){
+        if (a === b){
+            count++;
+            $("#level-title").text("Level "+count);
+        }else{
+            $("#level-title").text("Game over. Press any key to start.");
+            $("button").addClass("game-over");
+        }
+    
+    }
+    
